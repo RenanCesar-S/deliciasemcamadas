84fd111c8a6353d1ed205ac4f3c63d9e884bdf5d
Renan C. Silva <renan_cesar2000@hotmail.com>
2024-07-10 15:28:32 -0300

    fix close button on gallery; change second photo on about page

--- pages/about-script.js
+++ pages/about-script.js
@@ -1,6 +1,6 @@
 const lastImg = document.querySelector(".bg-for-large-device img");
 
 let screenWidth = window.innerWidth
-    if(screenWidth > 600 && screenWidth < 992) {
+    if(screenWidth > 600 && screenWidth < 1280) {
         lastImg.src = "../imgs/gallery/happy-cup-candy.jpeg"
     }
